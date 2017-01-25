5301a3bc2d02176470fcfe0fb4c6572162289803
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2017-01-25 12:11:34 -0600

    Bibliotecas
    - Se agregó herramientas de Firebase.

---
+++ app/src/main/java/com/shellcore/android/messaging/MessagingApplication.java
@@ -0,0 +1,18 @@
+package com.shellcore.android.messaging;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by Cesar on 25/01/2017.
+ */
+
+public class MessagingApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
