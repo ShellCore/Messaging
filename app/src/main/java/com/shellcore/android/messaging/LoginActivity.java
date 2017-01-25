abcbdb3ffddfd763df712a1bc9a270504564f97a
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2017-01-25 12:18:14 -0600

    Inyección de vista
    - Se agregaron los elementos de la vista de Login en el controlador Java con Butterknife.

--- app/src/main/java/com/shellcore/android/messaging/LoginActivity.java
+++ app/src/main/java/com/shellcore/android/messaging/LoginActivity.java
@@ -1,13 +1,45 @@
 package com.shellcore.android.messaging;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.edtEmail)
+    EditText edtEmail;
+    @Bind(R.id.tilEmail)
+    TextInputLayout tilEmail;
+    @Bind(R.id.edtPassword)
+    EditText edtPassword;
+    @Bind(R.id.tilPassword)
+    TextInputLayout tilPassword;
+    @Bind(R.id.lnrButtons)
+    LinearLayout lnrButtons;
+    @Bind(R.id.barLogin)
+    ProgressBar barLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick
+    public void handleSignin() {
+
+    }
+
+    @OnClick
+    public void handleSignup() {
+
     }
 }
