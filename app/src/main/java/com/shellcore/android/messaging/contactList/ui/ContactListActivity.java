2d7144bd78a154d5a490d960f35b5025b8eba8da
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2017-02-13 10:21:40 -0600

    Adaptadores
    - Se creó el adaptador de la lista de usuarios relacionados con el usuario loggeado.

--- app/src/main/java/com/shellcore/android/messaging/contactList/ui/ContactListActivity.java
+++ app/src/main/java/com/shellcore/android/messaging/contactList/ui/ContactListActivity.java
@@ -1,15 +1,81 @@
 package com.shellcore.android.messaging.contactList.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.shellcore.android.messaging.R;
+import com.shellcore.android.messaging.contactList.ContactListPresenter;
+import com.shellcore.android.messaging.entities.User;
 
-public class ContactListActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class ContactListActivity extends AppCompatActivity implements ContactListView {
+
+    // Servicios
+    private ContactListPresenter presenter;
+
+    // Componentes
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.recContactList)
+    RecyclerView recContactList;
+    @Bind(R.id.activity_contact_list)
+    CoordinatorLayout activityContactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+        ButterKnife.bind(this);
+
+        presenter.onCreate();
+        toolbar.setTitle(presenter.getCurrentUserEmail());
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    protected void onPause() {
+        presenter.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @OnClick(R.id.btnAdd)
+    public void addContact() {
+
+    }
+
+    @Override
+    public void onContactAdded(User user) {
+
+    }
+
+    @Override
+    public void onContactChanged(User user) {
+
+    }
+
+    @Override
+    public void onContactRemoved(User user) {
+
     }
 }
---
+++ app/src/main/java/com/shellcore/android/messaging/contactList/ui/adapters/ImageLoader.java
@@ -0,0 +1,10 @@
+package com.shellcore.android.messaging.contactList.ui.adapters;
+
+import android.widget.ImageView;
+
+/**
+ * Created by Cesar on 31/01/2017.
+ */
+public interface ImageLoader {
+    public void load(ImageView imgContact, String url);
+}
