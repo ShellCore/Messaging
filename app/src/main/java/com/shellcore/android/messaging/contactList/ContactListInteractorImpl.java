d795c6f1da8d58490bc1603d37209d4153ff1a6d
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2017-02-13 11:01:34 -0600

    Implementación Presentador.
    - Se creó la implementación del presentador de la lista de usuarios.
    - Se agregaron estados en el control de eventos de la lista.

---
+++ app/src/main/java/com/shellcore/android/messaging/contactList/ContactListInteractorImpl.java
@@ -0,0 +1,26 @@
+package com.shellcore.android.messaging.contactList;
+
+/**
+ * Created by Cesar on 13/02/2017.
+ */
+public class ContactListInteractorImpl implements ContactListInteractor {
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void unsubscribe() {
+
+    }
+
+    @Override
+    public void destroyListener() {
+
+    }
+
+    @Override
+    public void removeContact(String email) {
+
+    }
+}
---
+++ app/src/main/java/com/shellcore/android/messaging/contactList/ContactListSessionInteractorImpl.java
@@ -0,0 +1,21 @@
+package com.shellcore.android.messaging.contactList;
+
+/**
+ * Created by Cesar on 13/02/2017.
+ */
+public class ContactListSessionInteractorImpl implements ContactListSessionInteractor {
+    @Override
+    public void signoff() {
+
+    }
+
+    @Override
+    public String getCurrentUserEmail() {
+        return null;
+    }
+
+    @Override
+    public void changeConnectionStatus(boolean online) {
+
+    }
+}
