565a8b12fb5476828029f5e813c9f59e52eedaca
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2017-02-14 16:54:47 -0600

    Implementación de interfaces Agregar contacto
    - Se implementaron las interfaces del modelo MVP/Clean de la función para agregar un contacto.

--- app/src/main/java/com/shellcore/android/messaging/addContact/AddContactPresenterImpl.java
+++ app/src/main/java/com/shellcore/android/messaging/addContact/AddContactPresenterImpl.java
@@ -2,6 +2,10 @@ package com.shellcore.android.messaging.addContact;
 
 import com.shellcore.android.messaging.addContact.events.AddContactEvent;
 import com.shellcore.android.messaging.addContact.ui.AddContactView;
+import com.shellcore.android.messaging.libs.EventBus;
+import com.shellcore.android.messaging.libs.GreenRobotEventBus;
+
+import org.greenrobot.eventbus.Subscribe;
 
 /**
  * Created by Cesar on 14/02/2017.
@@ -11,27 +15,48 @@ public class AddContactPresenterImpl implements AddContactPresenter {
     // Variables
     private AddContactView view;
 
+    // Servicios
+    private EventBus eventBus;
+    private AddContactInteractor interactor;
+
     public AddContactPresenterImpl(AddContactView view) {
         this.view = view;
+        this.eventBus = GreenRobotEventBus.getInstance();
+        this.interactor = new AddContactInteractorImpl();
     }
 
     @Override
     public void onShow() {
-
+        eventBus.register(this);
     }
 
     @Override
     public void onDestroy() {
-
+        view = null;
+        eventBus.unregister(this);
     }
 
     @Override
     public void addContact(String email) {
-
+        if (view != null) {
+            view.hideInput();
+            view.showProgress();
+        }
+        interactor.execute(email);
     }
 
+    @Subscribe
     @Override
     public void onEventMainThread(AddContactEvent event) {
+        if (view != null) {
+            view.hideProgress();
+            view.showInput();
 
+            if (event.isError()) {
+                view.contactNotAdded();
+            } else {
+                view.contactAdded();
+            }
+        }
     }
 }
--- app/src/main/java/com/shellcore/android/messaging/addContact/events/AddContactEvent.java
+++ app/src/main/java/com/shellcore/android/messaging/addContact/events/AddContactEvent.java
@@ -4,5 +4,13 @@ package com.shellcore.android.messaging.addContact.events;
  * Created by Cesar on 14/02/2017.
  */
 public class AddContactEvent {
+    boolean error = false;
 
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
 }
