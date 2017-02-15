d99662251468ed1eebb1beb8513e1766e1b2cdb5
Cesar Morales Garduño <shell.one.core@gmail.com>
2017-02-15 16:10:22 -0600

    Creación de la vista de creación de nueva cuenta.

---
+++ app/src/main/java/com/shellcore/android/messaging/createAccount/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.shellcore.android.messaging.createAccount.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.shellcore.android.messaging.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
